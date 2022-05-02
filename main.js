9d9a805141209b7699e82e0eb1ac337d0af8fdc2
GayathriKrishna2010 <91413553+GayathriKrishna2010@users.noreply.github.com>
2022-05-02 16:36:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 550);
+  video.position(50 , 250)
+
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized!');
+}
+
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " + noseX +" noseY = " + noseY);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
+}
+
+function draw() {
+    background('#2e2d2d');
+    
+      document.getElementById("square_side").innerHTML = "Font size of the text will be = " + difference +"px";
+    textSize(difference);
+    fill('#000000');
+    text('Font', 50, 400);
+ }
